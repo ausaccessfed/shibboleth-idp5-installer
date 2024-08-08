9f43353dd97d54278d48878959b5b6ad1e99bc93
Terry Smith <t.smith@aaf.edu.au>
2024-08-08 06:34:48 +0000

    Ready to move to develop

--- bootstrap-v5.sh
+++ bootstrap-v5.sh
@@ -37,7 +37,7 @@ function set_internal_variables {
 
     GIT_REPO=https://github.com/ausaccessfed/shibboleth-idp5-installer.git
     GIT_BRANCH=main
-    GIT_BRANCH=feature-move-to-v5
+    GIT_BRANCH=develop
 
     FM_TEST_REG=https://manager.test.aaf.edu.au/
     FM_PROD_REG=https://manager.aaf.edu.au/
